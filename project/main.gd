4b6f91dc2b69168d6f84bed632aaa6a5b09bf5d3
Banderi <banderi002@gmail.com>
2021-09-15 09:40:24 +0200

    Revert to original script code

--- project/main.gd
+++ project/main.gd
@@ -5,6 +5,4 @@ const SIMPLE = preload("res://gdnative/simple.gdns")
 onready var data = SIMPLE.new()
 
 func _on_Button_pressed():
-	while (true):
-		data.get_data()
-#	$Label.text = "Data = " + data.get_data()
+	$Label.text = "Data = " + data.get_data()
